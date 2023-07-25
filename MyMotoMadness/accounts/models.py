bad13b281f7f280b9a6c807a61ae224fcf180d89
Yanko Rashkov <thewhitedead@abv.bg>
2023-07-26 00:23:35 +0300

    update

--- MyMotoMadness/accounts/models.py
+++ MyMotoMadness/accounts/models.py
@@ -1,3 +1,12 @@
-from django.db import models
-
-# Create your models here.
+# from django.contrib.auth import models as auth_models
+# from django.db import models
+#
+#
+# # Create your models here.
+# class MotoUserModel(auth_models.AbstractUser):
+#     first_name = models.CharField(
+#         max_length=30,
+#     )
+#     last_name = models.CharField(
+#         max_length=30,
+#     )
--- MyMotoMadness/settings.py
+++ MyMotoMadness/settings.py
@@ -113,3 +113,4 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#AUTH_USER_MODEL = 'accounts.MotoUserModel'
