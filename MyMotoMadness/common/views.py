aadfcabdf8bf6e21ff15a391a9e6cc5cd907e71e
Yanko Rashkov <thewhitedead@abv.bg>
2023-08-06 23:34:57 +0300

    update common view

--- MyMotoMadness/common/views.py
+++ MyMotoMadness/common/views.py
@@ -1,6 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.views import generic as views
+
+from MyMotoMadness.saleads.models import Motorcycles, MotoParts
+
+UserModel = get_user_model()
 
 
 # Create your views here.
 class IndexView(views.TemplateView):
     template_name = 'core/index.html'
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['bikes_quantity'] = Motorcycles.objects.all().count()
+        data['equipment_quantity'] = MotoParts.objects.all().count()
+        data['parts_quantity'] = MotoParts.objects.all().count()
+        data['registered_users'] = UserModel.objects.all().count()
+
+        return data
