e47539170764e6280ebcc8c49e8c9ddd0f8ef6ab
Yanko Rashkov <thewhitedead@abv.bg>
2023-08-12 20:20:34 +0300

    Update

--- MyMotoMadness/settings.py
+++ MyMotoMadness/settings.py
@@ -1,21 +1,13 @@
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.getenv('DEBUG')))
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(' ')
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
